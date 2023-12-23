0ab5cb0b8dd6c68d2ab04f5ec1c5cb9fe2dc7fd5
saurabhprajapati <56864338+prajapatisaurabh@users.noreply.github.com>
2023-12-23 15:05:03 +0530

    rework for pagination

--- pagination-example/src/App.tsx
+++ pagination-example/src/App.tsx
@@ -1,26 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import InfiniteScroll from "react-infinite-scroll-component";
+import "./App.css";
+import { useState } from "react";
 
-function App() {
+const App = () => {
+  const [state, setState] = useState<string[]>([]);
+  const [hasMore, setHasMore] = useState(true);
+
+  const fetchMoreData = () => {
+    // a fake async api call which sends
+    // 20 more records in 1.5 secs
+    setTimeout(() => {
+      const newData = Array.from(
+        { length: 20 },
+        (_, index) => `Item #${state.length + index}`
+      );
+      setState((prev) => [...prev, ...newData]);
+
+      // If newData length is less than 20, it means no more data to fetch
+      if (newData.length < 20) {
+        setHasMore(false);
+      }
+    }, 1500);
+  };
+
+  const style = {
+    height: 30,
+    border: "1px solid green",
+    margin: 6,
+    padding: 8,
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div>
+        <h1>demo: react-infinite-scroll-component</h1>
+        <hr />
+        <InfiniteScroll
+          dataLength={state.length}
+          next={fetchMoreData}
+          hasMore={hasMore}
+          loader={<h4>Loading...</h4>}
         >
-          Learn React
-        </a>
-      </header>
+          {state.map((item, index) => (
+            <div style={style} key={index}>
+              {index}_{item}
+            </div>
+          ))}
+        </InfiniteScroll>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
