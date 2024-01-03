fddbd28d46360743879b5ebc4043e7cff38f863f
saurabhprajapati <56864338+prajapatisaurabh@users.noreply.github.com>
2024-01-03 08:36:37 +0530

    CREATE - vite app for view - galary component

---
+++ view-galary/src/App.tsx
@@ -0,0 +1,18 @@
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+const App = () => {
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <h2> Hello World</h2>
+    </>
+  );
+};
+
+export default App;
